51e7497bb8e2187e196f87a9d93e3260051eb863
Derhks <derhks@gmail.com>
2019-12-10 22:54:39 +0000

    [Fix] logic update

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-
 for cnt in range(0, 100):
     if cnt != 99:
         print("{:02d}, ".format(cnt), end='')
     else:
-        print("{:d} ".format(cnt))
+        print("{:d}".format(cnt))
