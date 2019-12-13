49526ef77f397b2ac5d5f9bf6fe0bb07b90e9e27
Derhks <derhks@gmail.com>
2019-12-13 03:17:03 +0000

    [Fix] the code so that it doesn't execute when imported

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 
-import hidden_4
-for lst in dir(hidden_4):
-    line = "__"
-    if lst[:2] != line:
-        print("{:s}".format(lst))
-    else:
-        continue
+if __name__ == "__main__":
+    import hidden_4
+    for lst in dir(hidden_4):
+        line = "__"
+        if lst[:2] != line:
+            print("{:s}".format(lst))
+        else:
+            continue
