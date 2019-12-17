54eb5c41c687e4c43ced57657cbcff0fb6ea70cb
Derhks <derhks@gmail.com>
2019-12-17 22:52:12 +0000

    [Delete] wrong file

--- 0x03-python-data_structures/5-no_c.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-def no_c(my_string):
-    my_list = list(my_string)
-    for cnt in range(0, len(my_string)):
-        if my_list[cnt] == 'c' or my_list[cnt] == 'C':
-            my_list[cnt] = ""
-    new_string = "".join(my_list)
-    return new_string
