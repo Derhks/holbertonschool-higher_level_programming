77f700760952e57574dabfc798438d2563068b10
Derhks <derhks@gmail.com>
2019-12-18 00:04:06 +0000

    [Fix] delete space after {}

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,7 +2,7 @@
 def print_matrix_integer(matrix=[[]]):
     for cnt in matrix:
         for cnt1 in cnt:
-            print("{:d} ".format(cnt1), end="")
+            print("{:d}".format(cnt1), end="")
             if cnt1 != cnt[-1]:
                 print(end=' ')
         print()
