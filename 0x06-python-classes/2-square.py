e5ff499c00b030ca89a46d11e0cbe08c607d1832
Derhks <derhks@gmail.com>
2020-01-10 01:35:45 +0000

    [Fix] pep8, lines 10 and 11

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -7,7 +7,7 @@ class Square:
     def __init__(self, size=0):
         if isinstance(size, int):
             if size < 0:
-               raise ValueError("size must be >= 0")
-            self.__size = size # Private instance attribute
+                raise ValueError("size must be >= 0")
+            self.__size = size  # Private instance attribute
         else:
             raise TypeError("size must be an integer")
