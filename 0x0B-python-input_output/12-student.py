dbd822c5f33967409788063f5576bb46225867b1
Derhks <derhks@gmail.com>
2020-01-22 03:23:53 +0000

    [Create] a class Student that defines a student

---
+++ 0x0B-python-input_output/12-student.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+
+class Student:
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        new_dict = {}
+        if isinstance(attrs, list):
+            for name in attrs:
+                if isinstance(name, str):
+                    for dir_names in self.__dict__:
+                        if name == dir_names:
+                            new_dict[name] = self.__dict__[dir_names]
+            return new_dict
+        else:
+            return self.__dict__.copy()
