8e13cc3fb2b97b386c8feabe271efa1b4ec14be5
Derhks <derhks@gmail.com>
2020-01-28 03:11:27 +0000

    [Fix] line 12, wrong indente block

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -9,7 +9,7 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         """This method initialize all the instance"""
 
-    super().__init__(size, size, x, y, id)
+        super().__init__(size, size, x, y, id)
 
     def __str__(self):
         """This method return
