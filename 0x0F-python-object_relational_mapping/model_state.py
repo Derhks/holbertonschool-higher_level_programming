c3e622c0efd943356de21a29b37caa221ab0e196
Derhks <derhks@gmail.com>
2020-03-29 21:36:34 +0000

    [Delete] unnecessary statements

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -6,8 +6,8 @@ from sqlalchemy import create_engine
 Base = declarative_base()
 
 
-class State(Base):
-    __tablename__ = 'states'
-    id = Column(Integer, nullable=False, autoincrement=True,
+class State(Base):  # inherits from Base
+    __tablename__ = 'states'  # links to the MySQL table states
+    id = Column(autoincrement=True, Integer, nullable=False,
                 primary_key=True)
     name = Column(String(128), nullable=False)
