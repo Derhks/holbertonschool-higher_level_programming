65a4b99bdef555a3769a0841e69c40f6369adefb
Derhks <derhks@gmail.com>
2020-03-29 17:26:31 +0000

    [Create] a script that takes in the name of a state and lists all cities

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    username = argv[1]
+    password = argv[2]
+    db_name = argv[3]
+    state_name = argv[4]
+
+    # Open database connection
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=username,
+                         passwd=password,
+                         db=db_name)
+
+    # prepare a cursor object using cursor() method
+    cur = db.cursor()
+
+    cur.execute("SELECT cities.name FROM cities, states\
+                WHERE states.name = %s AND cities.state_id = states.id\
+                ORDER BY cities.id ASC", (state_name, ))
+    rows = cur.fetchall()
+    cnt = 0
+    for row in rows:
+        for city in row:
+            if cnt <= len(row):
+                print("{:s}".format(city), end=", ")
+            else:
+                print("{:s}".format(city), end="")
+            cnt += 1
+    print()
+
+    cur.close()
+    db.close()
