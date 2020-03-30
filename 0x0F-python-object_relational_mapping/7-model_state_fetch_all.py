2598239a8a28791b1486dc6989fe0b55dcf50415
Derhks <derhks@gmail.com>
2020-03-30 03:19:11 +0000

    [Update] add conectivity with the database

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -13,6 +13,7 @@ if __name__ == "__main__":
     engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".
                            format(username, password, db_name),
                            encoding='latin1', echo=True, pool_pre_ping=True)
+    Base.metadata.create_all(engine)
 
     Session = sessionmaker(bind=engine)
     session = Session()
