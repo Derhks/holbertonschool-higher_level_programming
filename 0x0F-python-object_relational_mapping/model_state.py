45227a59317c22e684c9c10ca6ab43f588fb1822
Derhks <derhks@gmail.com>
2020-03-30 00:08:33 +0000

    [Feature] add permissions execute

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python3
+"""Firt state model"""
+
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -5,6 +8,7 @@ Base = declarative_base()
 
 
 class State(Base):
+    """ Class of states table """
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
