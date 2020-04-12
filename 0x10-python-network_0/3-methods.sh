f8879e2ebbbc16b695a9914c45d7081fb0942b62
Derhks <derhks@gmail.com>
2020-04-12 23:28:09 +0000

    [Feature] using cut

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #  This script display the methods the server will accept
-curl -sI OPTIONS "$1" | grep Allow: | awk '{print $2 " "$3 " "$4}'
+curl -sI OPTIONS "$1" | grep Allow: | cut -f 2-4 -d" "
