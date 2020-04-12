80066535b5bbd8d73ebb27832a17cbd3a5cd9e3b
Derhks <derhks@gmail.com>
2020-04-12 23:36:57 +0000

    [Update] new logical

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #  This script display the methods the server will accept
-curl -sI -X OPTIONS "$1" | grep Allow: | cut -f 2-4 -d" "
+curl -sI OPTIONS "$1" | grep Allow: | cut -d' ' -f2-
