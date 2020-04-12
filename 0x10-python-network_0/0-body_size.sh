8c9a5011868ed91342ad1eac49024cdd97131e92
Derhks <derhks@gmail.com>
2020-04-12 21:00:20 +0000

    [Fix] delete a flag to create a file

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #  This script display the body size of a website
-curl -s $1 -o test | awk '{ print $NF }' test | head -3 | tail -1
+curl -sI $1 | awk '{ print $NF }' | head -3 | tail -1
