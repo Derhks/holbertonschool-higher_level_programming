4da666fce02e5a0d837f96332fff25e27236cba8
Derhks <derhks@gmail.com>
2020-04-13 03:39:01 +0000

    [Feature] using recursion to find the peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,17 +1,14 @@
 #!/usr/bin/python3
 
 
-def find_peak(list_of_integers):
+def find_peak(list_of_integers, itr=0, value=0):
     """This method find the peak of the list"""
     if len(list_of_integers) > 0:
-        value = list_of_integers[0]
-        max_value = value
-        for cnt in range(len(list_of_integers)):
-            if list_of_integers[cnt] > value:
-                value = list_of_integers[cnt]
-                for cnt2 in range(len(list_of_integers)):
-                    if list_of_integers[cnt2] > value:
-                        value = list_of_integers[cnt2]
+        value = value
+        if list_of_integers[itr] > value:
+            value = list_of_integers[itr]
+        if itr < len(list_of_integers) - 1:
+            return find_peak(list_of_integers, itr + 1, value)
         return value
     else:
         return None
