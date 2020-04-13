9638453c0742f6ffab59b54cc97463df4d4be5e2
Derhks <derhks@gmail.com>
2020-04-13 03:47:05 +0000

    [Feature] using binary tree to find the peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
 
 
-def find_peak(list_of_integers, itr=0, value=0):
+def find_peak(list_of_integers, itr=0, value=0, rev_itr=0):
     """This method find the peak of the list"""
     if len(list_of_integers) > 0:
+        rev_itr = len(list_of_integers) - 1
         value = value
+        if list_of_integers[rev_itr] > value:
+            value = list_of_integers[rev_itr]
         if list_of_integers[itr] > value:
             value = list_of_integers[itr]
         if itr < len(list_of_integers) - 1:
-            return find_peak(list_of_integers, itr + 1, value)
+            return find_peak(list_of_integers, itr + 1, value, rev_itr - 1)
         return value
     else:
         return None
