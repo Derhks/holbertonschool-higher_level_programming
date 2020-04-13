380e971d707509faa112da26faba1a1d18925705
Derhks <derhks@gmail.com>
2020-04-13 04:06:10 +0000

    [Update] two more if statement

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -3,7 +3,11 @@
 
 def find_peak(list_of_integers, itr=0, value=0, rev_itr=0):
     """This method find the peak of the list"""
-    if len(list_of_integers) > 0:
+    if len(list_of_integers) == 1:
+        return list_of_integers[0]
+    if len(list_of_integers) == 2:
+        return max(list_of_integers)
+    if len(list_of_integers) > 2:
         rev_itr = len(list_of_integers) - 1
         value = value
         if list_of_integers[rev_itr] > value:
