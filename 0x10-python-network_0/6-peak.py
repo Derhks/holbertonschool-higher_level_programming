168cc8e2181347c2436e4815c1451d94a826cc2f
Derhks <derhks@gmail.com>
2020-04-13 04:45:30 +0000

    [Delete] delete yappi

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
-import yappi
-"""
-Function that finds a peak in a list of unsorted integers.
-"""
+
 
 def find_peak(list_of_integers):
+    """This method find the peak of the list"""
     if list_of_integers:
         lo = 0
         hi = len(list_of_integers)
@@ -24,8 +22,3 @@ def find_peak(list_of_integers):
                 return find_peak(list_of_integers[:mi])
     else:
         return None
-
-yappi.start() # Iniciamos la monitorización de yappi
-find_peak([1, 2, 6, 4, 3]) # Llamada a la funcion de ejemplo
-yappi.get_func_stats().print_all() # Mostramos las stats de ejecución
-yappi.stop() # Paramos la monitorización de yappi
