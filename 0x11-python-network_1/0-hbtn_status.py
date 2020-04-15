d4b5a19ed1dfc5e161ee1616ceb9197232ef466b
Derhks <derhks@gmail.com>
2020-04-15 23:04:36 +0000

    [Change] import statement

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-import urllib.request
 """What's my status?"""
 
 
 if __name__ == "__main__":
+    import urllib.request
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         answer = response.read()
-    print("Body response:")
-    print("\t- type: {}".format(type(answer)))
-    print("\t- content: {}".format(answer))
-    print("\t- uft8 content: {}".format(answer.decode("utf-8")))
+        print("Body response:")
+        print("\t- type: {}".format(type(answer)))
+        print("\t- content: {}".format(answer))
+        print("\t- uft8 content: {}".format(answer.decode("utf-8")))
