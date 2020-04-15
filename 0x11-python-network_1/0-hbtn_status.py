50850e256087178303235c75734ee675b66a1b28
Derhks <derhks@gmail.com>
2020-04-15 23:09:50 +0000

    [Change] quotes

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,7 +4,7 @@
 
 if __name__ == "__main__":
     import urllib.request
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+    with urllib.request.urlopen("https://intranet.hbtn.io/status") as response:
         answer = response.read()
         print("Body response:")
         print("\t- type: {}".format(type(answer)))
