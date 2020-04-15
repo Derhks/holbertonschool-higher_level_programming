4b72db665854f4b4bfe56251f79cb56e7ed0fed8
Derhks <derhks@gmail.com>
2020-04-15 23:15:58 +0000

    [Change] die Bart die

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
-"""What's my status?"""
-
-
+"""Show the body of the header response"""
 if __name__ == "__main__":
     import urllib.request
-    with urllib.request.urlopen("https://intranet.hbtn.io/status") as response:
-        answer = response.read()
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         print("Body response:")
+        answer = response.read()
         print("\t- type: {}".format(type(answer)))
         print("\t- content: {}".format(answer))
-        print("\t- uft8 content: {}".format(answer.decode("utf-8")))
+        print("\t- utf8 content: {}".format(answer.decode('utf-8')))
