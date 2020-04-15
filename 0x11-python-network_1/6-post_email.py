416f1b2253f6ac1148a4f381114797d3e5c01bb2
Derhks <derhks@gmail.com>
2020-04-15 23:58:37 +0000

    [Fix] pep8

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -4,5 +4,5 @@ import sys
 
 
 if __name__ == "__main__":
-    webpage = requests.post(sys.argv[1], data = {'email': sys.argv[2]})
+    webpage = requests.post(sys.argv[1], data={'email': sys.argv[2]})
     print(webpage.text)
