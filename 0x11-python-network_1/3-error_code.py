8745de1147d43851eafc737e156039c651443756
Derhks <derhks@gmail.com>
2020-04-15 22:37:02 +0000

    [Create] a Python script that takes in a URL, sends a request to the URL

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,11 @@
+import urllib.request
+import sys
+
+
+if __name__ == "__main__":
+        try:
+            request = urllib.request.Request(sys.argv[1])
+            with urllib.request.urlopen(request) as response:
+                print(response.read().decode('utf-8'))
+        except urllib.error.HTTPError as error:
+            print("Error code: {:d}".format(error.code))
