cdfc73066d318944f6f164c369efdb4f2cd6b229
Derhks <derhks@gmail.com>
2020-04-18 19:00:41 +0000

    [Change] using the put method

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #  This script make a request but the response message is You got me!
-curl -sL 0.0.0.0:5000/catch_me -o /dev/null -w "You got me!"
+curl -sL OPTIONS 0.0.0.0:5000/catch_me -X PUT -H "origin: HolbertonSchool" -d "user_id=98"
