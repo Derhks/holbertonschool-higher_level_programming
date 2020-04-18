25744d92127ca281d3e3d6c13b20a648f74de363
Derhks <derhks@gmail.com>
2020-04-18 15:14:27 +0000

    [Change] The call to the json file

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #  This script send a JSON POST request
-curl -X POST -H "Content-Type: application/json" -d @./"$2" "$1"
+curl -X POST -H "Content-Type: application/json" -d @"$2" "$1"
