630adc81f0a83050203fe178377828d7817b6b09
Derhks <derhks@gmail.com>
2020-05-05 22:31:32 +0000

    [Feature] improving the code

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -3,16 +3,13 @@ let total = 1;
 function factorial (numb) {
   if (numb === 0) {
     return 1;
+  } else if (isNaN(numb)) {
+    return 1;
+  } else {
+    total = total * numb;
+    factorial(numb - 1);
+    return total;
   }
-  total = total * numb;
-  factorial(numb - 1);
-  return total;
 }
 const numb = parseInt(process.argv[2]);
-let fact = 0;
-if (isNaN(numb)) {
-  console.log(1);
-} else {
-  fact = factorial(numb);
-  console.log(fact);
-}
+console.log(factorial(numb));
