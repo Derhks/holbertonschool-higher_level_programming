1037639b0aa396a3535c74f1fcfad24cd8b2183d
Derhks <derhks@gmail.com>
2020-05-05 16:40:19 +0000

    [Create] a script that display severals messages

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+let cnt = 0;
+const lenguages = ['C is fun', 'Python is coolw', 'Javascript is amazing'];
+for (cnt = 0; cnt < lenguages.length; cnt++) {
+  console.log(lenguages[cnt]);
+}
