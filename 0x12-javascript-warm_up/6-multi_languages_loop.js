81030fed9b357d324decf61440a6cd52e0c1ace3
Derhks <derhks@gmail.com>
2020-05-05 22:00:01 +0000

    [Fix] no comments

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 let cnt = 0;
-const lenguages = ['C is fun', 'Python is coolw', 'Javascript is amazing'];
+const lenguages = ['C is fun', 'Python is cool', 'Javascript is amazing'];
 for (cnt = 0; cnt < lenguages.length; cnt++) {
   console.log(lenguages[cnt]);
 }
