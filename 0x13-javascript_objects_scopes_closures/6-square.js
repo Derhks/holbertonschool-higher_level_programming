a2b2dc10a4abe534d6caafa89ac22e490ce4daf1
Derhks <derhks@gmail.com>
2020-05-11 21:14:09 +0000

    [Update] without super

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -3,20 +3,15 @@ const squareV1 = require('./5-square');
 
 module.exports = class Square extends squareV1 {
   charPrint (c) {
-    let cnt = 0;
     let print = '';
     if (c === undefined) {
       c = 'X';
     }
-    if (c === 'C') {
-      for (cnt = 0; cnt < this.width; cnt++) {
-        print += c;
-      }
-      for (cnt = 0; cnt < this.height; cnt++) {
-        console.log(print);
-      }
-    } else {
-      super.print();
+    for (let cnt = 0; cnt < this.width; cnt++) {
+      print += c;
+    }
+    for (let cnt = 0; cnt < this.height; cnt++) {
+      console.log(print);
     }
   }
 };
