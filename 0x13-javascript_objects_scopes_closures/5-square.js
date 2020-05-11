c1fe4809e541a597f73ca00f9bfe56b99538906b
Derhks <derhks@gmail.com>
2020-05-11 19:03:20 +0000

    [Create] a script that contains a square class and inherits it from a rectangle

---
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -0,0 +1,42 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w <= 0 || h <= 0 || w === undefined || h === undefined) {
+      return undefined;
+    } else {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let cnt = 0;
+    let print = '';
+    for (cnt = 0; cnt < this.width; cnt++) {
+      print += 'X';
+    }
+    for (cnt = 0; cnt < this.height; cnt++) {
+      console.log(print);
+    }
+  }
+
+  rotate () {
+    const tmpWidth = this.width;
+    const tmpHeight = this.height;
+    this.width = tmpHeight;
+    this.height = tmpWidth;
+  }
+
+  double () {
+    const doubleWidth = this.width * 2;
+    const doubleHeight = this.height * 2;
+    this.width = doubleWidth;
+    this.height = doubleHeight;
+  }
+}
+
+module.exports = class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+};
