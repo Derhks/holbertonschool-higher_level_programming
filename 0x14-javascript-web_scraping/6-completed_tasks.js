4915b29bf477e057f57683640540be77ee3ac2ef
Derhks <derhks@gmail.com>
2020-05-12 20:08:02 +0000

    [Update] using return

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -4,15 +4,14 @@ const request = require('request');
 request(process.argv[2], (error, response, body) => {
   if (error === null) {
     const listBody = JSON.parse(body);
-    const lenList = listBody.length;
     const dictTasks = {};
     let maxId = 0;
-    for (let itr2 = 0; itr2 < lenList; itr2++) {
+    for (let itr2 = 0; itr2 < listBody.length; itr2++) {
       maxId = listBody[itr2].userId;
     }
     for (let itr1 = 1; itr1 <= maxId; itr1++) {
       let cnt = 0;
-      for (let itr2 = 0; itr2 < lenList; itr2++) {
+      for (let itr2 = 0; itr2 < listBody.length; itr2++) {
         if (listBody[itr2].userId === itr1) {
           if (listBody[itr2].completed === true) {
             cnt++;
@@ -21,7 +20,7 @@ request(process.argv[2], (error, response, body) => {
       }
       dictTasks[itr1] = cnt;
     }
-    console.log(dictTasks);
+    return console.log(dictTasks);
   } else {
     console.error(error);
   }
