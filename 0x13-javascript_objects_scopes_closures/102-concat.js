79f5cc14aad049f76c05d796f43bdf33498348e2
Derhks <derhks@gmail.com>
2020-05-12 15:37:21 +0000

    [Update] another method

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -3,11 +3,27 @@ const fs = require('fs');
 const f1 = process.argv[2];
 const f2 = process.argv[3];
 const f3 = process.argv[4];
-try {
-  const d1 = fs.readFileSync(f1, 'utf8');
-  const d2 = fs.readFileSync(f2, 'utf8');
-  const content = d1 + '\n' + d2 + '\n';
-  fs.writeFileSync(f3, content);
-} catch (err) {
-  console.error(err);
-}
+
+fs.readFile(f1, 'utf-8', (err, data) => {
+  if (err) {
+    return console.error(err);
+  } else {
+    fs.writeFile(f3, data, err => {
+      if (err) {
+        console.error(err);
+      }
+    });
+  }
+});
+
+fs.readFile(f2, 'utf-8', (err, data) => {
+  if (err) {
+    return console.error(err);
+  } else {
+    fs.appendFile(f3, data, err => {
+      if (err) {
+        console.error(err);
+      }
+    });
+  }
+});
