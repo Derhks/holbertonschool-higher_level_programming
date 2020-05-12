4528f4d45d63421f28ee0bbc430ca750283fc39b
Derhks <derhks@gmail.com>
2020-05-12 21:12:50 +0000

    [Create] a script that prints the number of movies where Wedge Antilles appear

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+const request = require('request');
+const url = process.argv[2];
+const wedgeAntilles = 'https://swapi-api.hbtn.io/api/people/18/';
+
+request(url, (error, response, body) => {
+  let cnt = 0;
+  if (error === null) {
+    for (let itr = 0; itr < JSON.parse(body).results.length; itr++) {
+      const listPeople = JSON.parse(body).results[itr].characters;
+      for (let itr2 = 0; itr2 < listPeople.length; itr2++) {
+        if (listPeople[itr2] === wedgeAntilles) {
+          cnt++;
+        }
+      }
+    }
+    console.log(cnt);
+  } else {
+    console.log(error);
+  }
+});
