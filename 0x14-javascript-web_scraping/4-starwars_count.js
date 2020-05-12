8dd30b1f2b8ccb39c8477e21ffadee22e640cfba
Derhks <derhks@gmail.com>
2020-05-12 23:45:21 +0000

    [Change] the if method

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
 const request = require('request');
 const url = process.argv[2];
-const wedgeAntilles = 'https://swapi-api.hbtn.io/api/people/18/';
 let cnt = 0;
 
 request(url, (error, response, body) => {
@@ -11,7 +10,7 @@ request(url, (error, response, body) => {
     for (let itr = 0; itr < JSON.parse(body).results.length; itr++) {
       const listPeople = JSON.parse(body).results[itr].characters;
       for (let itr2 = 0; itr2 < listPeople.length; itr2++) {
-        if (listPeople[itr2] === wedgeAntilles) {
+        if (listPeople[itr2].includes(18)) {
           cnt++;
         }
       }
