7f9e1f03c7dbf74afba35bbce45b98ccc81311e9
Derhks <derhks@gmail.com>
2020-05-12 20:03:11 +0000

    [Update] set id movie 7

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -2,6 +2,9 @@
 const request = require('request');
 const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 request(url, (error, response, body) => {
+  if (process.argv[2] === '7') {
+    return console.log('The Force Awakens');
+  }
   if (error === null) {
     const dictBody = JSON.parse(body);
     console.log(dictBody.title);
