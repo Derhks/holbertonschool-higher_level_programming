2f5e7e3f8ca0828a34e2764e9135152b8756ea3d
Derhks <derhks@gmail.com>
2020-05-12 17:17:28 +0000

    [Fix] delete return statement

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -4,7 +4,7 @@ const file = process.argv[2];
 
 fs.readFile(file, 'utf-8', (err, data) => {
   if (err) {
-    return console.error(err);
+    console.error(err);
   } else {
     console.log(data);
   }
